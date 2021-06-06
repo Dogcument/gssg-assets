144a3c87bb962a6b8aa62a120c6c98106cf1441e
choibyunghyeon <choi8608@gmail.com>
2021-06-06 21:43:27 +0900

    first commit

---
+++ src/test/java/com/gssg/gssgbe/GssgBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gssg.gssgbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GssgBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
