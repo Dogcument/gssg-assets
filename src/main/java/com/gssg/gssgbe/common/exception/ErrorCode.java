4d448f90f1c57b4d70d26aec0e8f797252808fb1
choibyunghyeon <choi8608@gmail.com>
2021-06-07 19:27:54 +0900

    GSSG-53 ♻️ 에러코드에 구분 문자 추가

--- src/main/java/com/gssg/gssgbe/common/exception/ErrorCode.java
+++ src/main/java/com/gssg/gssgbe/common/exception/ErrorCode.java
@@ -9,19 +9,19 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
   // 암호화
-  FAIL_ENCRYPT("0001", HttpStatus.CONFLICT, "암호화 실패"),
-  FAIL_DECRYPT("0002", HttpStatus.CONFLICT, "복호화 실패"),
+  FAIL_ENCRYPT("E0001", HttpStatus.CONFLICT, "암호화 실패"),
+  FAIL_DECRYPT("E0002", HttpStatus.CONFLICT, "복호화 실패"),
 
   // 인증
-  NOT_VALID_PASSWORD("1001", HttpStatus.NOT_FOUND, "일치하지 않는 암호"),
+  NOT_VALID_PASSWORD("E1001", HttpStatus.NOT_FOUND, "일치하지 않는 암호"),
 
   // 기타
-  BAD_REQUEST("0400", HttpStatus.BAD_REQUEST, "잘못된 입력 값"),
-  UNAUTHORIZED("0401", HttpStatus.UNAUTHORIZED, "인증 실패"),
-  FORBIDDEN("0403", HttpStatus.FORBIDDEN, "권한 없음"),
-  NOT_FOUND("0404", HttpStatus.NOT_FOUND, "찾을 수 없음"),
-  METHOD_NOT_ALLOWED("0405", HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메소드"),
-  UNKNOWN("0500", HttpStatus.INTERNAL_SERVER_ERROR, "알수 없는 서버 에러");
+  BAD_REQUEST("E0400", HttpStatus.BAD_REQUEST, "잘못된 입력 값"),
+  UNAUTHORIZED("E0401", HttpStatus.UNAUTHORIZED, "인증 실패"),
+  FORBIDDEN("E0403", HttpStatus.FORBIDDEN, "권한 없음"),
+  NOT_FOUND("E0404", HttpStatus.NOT_FOUND, "찾을 수 없음"),
+  METHOD_NOT_ALLOWED("E0405", HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 메소드"),
+  UNKNOWN("E0500", HttpStatus.INTERNAL_SERVER_ERROR, "알수 없는 서버 에러");
 
   private final String code;
   private final HttpStatus status;
