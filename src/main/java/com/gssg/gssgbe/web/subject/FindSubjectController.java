a21289a50a0a4531909a99a1132f4343fad557d8
hyune <choi8608@gmail.com>
2021-09-04 19:53:11 +0900

    GSSG-133 🐛 오타 수정

--- src/main/java/com/gssg/gssgbe/web/subject/FindSubjectController.java
+++ src/main/java/com/gssg/gssgbe/web/subject/FindSubjectController.java
@@ -1,30 +1,33 @@
 package com.gssg.gssgbe.web.subject;
 
+import java.time.LocalDate;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.gssg.gssgbe.common.exception.ErrorCode;
 import com.gssg.gssgbe.common.exception.custom.BusinessException;
 import com.gssg.gssgbe.domain.subject.service.FindSubjectOfDateService;
 import com.gssg.gssgbe.web.subject.response.SubjectResponse;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "글감")
 @RequiredArgsConstructor
 @RestController
 public class FindSubjectController {
 
-    private final FindSubjectOfDateService findSubjectOfDateService;
+	private final FindSubjectOfDateService findSubjectOfDateService;
 
-    @Operation(summary = "그날의 글감 조회")
-    @GetMapping("/api/v1/sujects/date/{targetDate}")
-    public SubjectResponse findByDate(
-        @PathVariable LocalDate targetDate) {
-        return findSubjectOfDateService.findByDate(targetDate)
-            .map(SubjectResponse::new)
-            .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
-    }
+	@Operation(summary = "그날의 글감 조회")
+	@GetMapping("/api/v1/subjects/date/{targetDate}")
+	public SubjectResponse findByDate(
+		@PathVariable final LocalDate targetDate) {
+		return findSubjectOfDateService.findByDate(targetDate)
+			.map(SubjectResponse::new)
+			.orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
+	}
 }
