d11cdfd1f88ef8507758f1fbd5dacddb527ba0b0
hyune <choi8608@gmail.com>
2021-09-05 14:57:46 +0900

    GSSG-151 ✨ 글 좋아요 토글 구현

--- src/main/java/com/gssg/gssgbe/domain/post/repository/PostLikeRepository.java
+++ src/main/java/com/gssg/gssgbe/domain/post/repository/PostLikeRepository.java
@@ -1,11 +1,16 @@
 package com.gssg.gssgbe.domain.post.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gssg.gssgbe.domain.member.entity.Member;
+import com.gssg.gssgbe.domain.post.entity.Post;
 import com.gssg.gssgbe.domain.post.entity.PostLike;
 
 @Repository
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
+	Optional<PostLike> findByPostAndMember(final Post post, final Member member);
 }
