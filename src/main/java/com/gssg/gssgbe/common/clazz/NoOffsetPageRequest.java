b96182a940c6ae4c03f1edc5494ed6bafa5607be
hyune <choi8608@gmail.com>
2021-09-30 22:09:22 +0900

    GSSG-166 ✨ noOffsetPageRequest 구현

---
+++ src/main/java/com/gssg/gssgbe/common/clazz/NoOffsetPageRequest.java
@@ -0,0 +1,29 @@
+package com.gssg.gssgbe.common.clazz;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Sort;
+
+import lombok.Getter;
+
+@Getter
+public class NoOffsetPageRequest {
+
+	private final long current;
+	private final int size;
+	private final Sort sort;
+
+	private NoOffsetPageRequest(final Long current, final int size, final Sort sort) {
+		this.current = Optional.ofNullable(current).orElse(Long.MAX_VALUE);
+		this.size = size;
+		this.sort = sort;
+	}
+
+	public static NoOffsetPageRequest of(final Long current, final int size) {
+		return of(current, size, Sort.unsorted());
+	}
+
+	public static NoOffsetPageRequest of(final Long current, final int size, final Sort sort) {
+		return new NoOffsetPageRequest(current, size, sort);
+	}
+}
