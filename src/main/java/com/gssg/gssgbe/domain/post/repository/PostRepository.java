ab37a0538eaf29c736b6a40e063349056a3c0097
hyune <choi8608@gmail.com>
2021-09-30 22:47:23 +0900

    GSSG-166 ♻️ noOffset 방식으로 post 조회 수정

--- src/main/java/com/gssg/gssgbe/domain/post/repository/PostRepository.java
+++ src/main/java/com/gssg/gssgbe/domain/post/repository/PostRepository.java
@@ -1,17 +1,11 @@
 package com.gssg.gssgbe.domain.post.repository;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gssg.gssgbe.domain.member.entity.Member;
 import com.gssg.gssgbe.domain.post.entity.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
-	Slice<Post> findAllSlice(Pageable pageable);
-
-	Slice<Post> findAllByMember(Member member, Pageable pageable);
 }
--- src/main/java/com/gssg/gssgbe/web/my/MyController.java
+++ src/main/java/com/gssg/gssgbe/web/my/MyController.java
@@ -1,10 +1,11 @@
 package com.gssg.gssgbe.web.my;
 
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Slice;
+import javax.annotation.Nullable;
+import javax.validation.constraints.Positive;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gssg.gssgbe.common.annotation.LoginMember;
+import com.gssg.gssgbe.common.clazz.NoOffsetPageRequest;
 import com.gssg.gssgbe.common.exception.ErrorCode;
 import com.gssg.gssgbe.common.exception.custom.BusinessException;
 import com.gssg.gssgbe.domain.member.dto.response.MemberDto;
@@ -73,11 +75,14 @@ public class MyController {
 	@GetMapping("/api/v1/my/posts")
 	public FindAllPostResponse findMyPosts(
 		@Parameter(hidden = true) @LoginMember final Member loginMember,
-		@RequestParam(defaultValue = "0") @PositiveOrZero final Integer page,
+		@RequestParam @Nullable @Positive final Long currentPostId,
 		@RequestParam(defaultValue = "10") @Positive final Integer size) {
-		final PageRequest pageRequest = PageRequest.of(page, size);
-		final Slice<PostDto> postDtos = findPostService.findByMember(loginMember, pageRequest);
+		final NoOffsetPageRequest pageRequest = NoOffsetPageRequest.of(currentPostId, size);
+		final List<PostDto> postDtos = findPostService.findByMember(loginMember, pageRequest);
 
-		return new FindAllPostResponse(postDtos.map(PostResponse::new));
+		return new FindAllPostResponse(
+			postDtos.stream()
+				.map(PostResponse::new)
+				.collect(Collectors.toList()));
 	}
 }
