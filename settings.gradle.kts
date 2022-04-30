6ad88b024441e1c5fc6dad153bf19e87551830bb
Taeyang Jin <sun@jupiterflow.com>
2022-05-01 03:04:08 +0900

    refactor: JPA 스크립트 최적화

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,6 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version kotlinVersion
-        kotlin("plugin.allopen") version kotlinVersion
-        kotlin("plugin.noarg") version kotlinVersion
-        kotlin("kapt") version kotlinVersion
-
-        kotlin("plugin.jpa") version kotlinVersion
         kotlin("plugin.spring") version kotlinVersion
         id("org.springframework.boot") version springBootVersion
         id("io.spring.dependency-management") version springDependencyManagementVersion
