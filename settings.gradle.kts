e9031776af7c5bf88815cc447860b0acb37d58da
Taeyang Jin <sun@jupiterflow.com>
2022-04-30 23:42:18 +0900

    chore: 코틀린 의존성 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,23 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.allopen") version kotlinVersion
+        kotlin("plugin.noarg") version kotlinVersion
+        kotlin("kapt") version kotlinVersion
+
+        kotlin("plugin.jpa") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagementVersion
+    }
+}
+
 rootProject.name = "gssg-assets"
+
 include("gssg-interface")
 include("gssg-adapter")
 include("gssg-application")
