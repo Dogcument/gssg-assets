a66debc9e27668ea5bc4ac2f437a4b7cbb8a8e55
Taeyang Jin <sun@jupiterflow.com>
2022-05-03 04:50:52 +0900

    feat: Member Persistence Layer resolve #19

--- gssg-adapter/out/persistence/src/main/kotlin/com/gssg/assets/persistence/domain/member/repository/MemberRepositoryImpl.kt
+++ gssg-adapter/out/persistence/src/main/kotlin/com/gssg/assets/persistence/domain/member/repository/MemberRepositoryImpl.kt
@@ -1,13 +1,20 @@
 package com.gssg.assets.persistence.domain.member.repository
 
+import com.gssg.assets.domain.member.MemberDisplayName
 import com.gssg.assets.domain.member.MemberId
 import com.gssg.assets.persistence.domain.member.entity.Member
+import com.gssg.assets.persistence.domain.member.entity.Members.displayName
 
 /**
  * @Author Heli
  */
 class MemberRepositoryImpl : MemberRepository {
-    override fun find(memberId: MemberId): Member? {
+
+    override fun findById(memberId: MemberId): Member? {
         return Member.findById(memberId.id)
+    }
+
+    override fun findByDisplayName(memberDisplayName: MemberDisplayName): Member? {
+        return Member.find { displayName eq memberDisplayName.displayName }.singleOrNull()
     }
 }
