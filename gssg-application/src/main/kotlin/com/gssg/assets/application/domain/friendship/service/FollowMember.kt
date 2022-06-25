cbd1ba7100057e825ff2e5afcc8f1dabc85abd12
Heli <sun@jupiterflow.com>
2022-06-26 04:04:00 +0900

    [GSSG-240] add friendship UseCase(FollowMember,UnFollowMember)
    close #35

---
+++ gssg-application/src/main/kotlin/com/gssg/assets/application/domain/friendship/service/FollowMember.kt
@@ -0,0 +1,26 @@
+package com.gssg.assets.application.domain.friendship.service
+
+import com.gssg.assets.application.domain.friendship.port.`in`.FollowMemberUseCase
+import com.gssg.assets.application.domain.friendship.port.out.FriendshipPersistencePort
+import com.gssg.assets.domain.logger
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+/**
+ * @Author Heli
+ */
+@Service
+class FollowMember(
+    private val friendshipPersistencePort: FriendshipPersistencePort
+) : FollowMemberUseCase {
+
+    private val logger = logger()
+
+    @Transactional
+    override fun command(command: FollowMemberUseCase.Command) {
+        val friendship = command.friendship
+
+        logger.info("Friendship 을 데이터베이스에 저장하기 위해 영속성 포트 호출 [Friendship.toMemberId=${friendship.toMemberId.toMemberId}, Friendship.fromMemberId=${friendship.fromMemberId.fromMemberId}]")
+        friendshipPersistencePort.insert(friendship)
+    }
+}
---
+++ gssg-application/src/main/kotlin/com/gssg/assets/application/domain/friendship/service/UnFollowMember.kt
@@ -0,0 +1,33 @@
+package com.gssg.assets.application.domain.friendship.service
+
+import com.gssg.assets.application.domain.friendship.port.`in`.UnFollowMemberUseCase
+import com.gssg.assets.application.domain.friendship.port.out.FriendshipPersistencePort
+import com.gssg.assets.application.domain.friendship.service.exception.FriendshipNotFoundException
+import com.gssg.assets.domain.logger
+import com.gssg.assets.domain.member.MemberId
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+/**
+ * @Author Heli
+ */
+@Service
+class UnFollowMember(
+    private val friendshipPersistencePort: FriendshipPersistencePort
+) : UnFollowMemberUseCase {
+
+    private val logger = logger()
+
+    @Transactional
+    override fun command(command: UnFollowMemberUseCase.Command) {
+        val friendshipId = command.friendshipId
+
+        val friendship = friendshipPersistencePort.findBy(id = friendshipId)
+            ?: throw FriendshipNotFoundException(friendshipId = friendshipId)
+
+
+        // TODO Authentication 정보 가져와 검증 필요
+        val temporaryRequestMemberId = MemberId(1L)
+        friendshipPersistencePort.update(friendship.unfollow(temporaryRequestMemberId))
+    }
+}
